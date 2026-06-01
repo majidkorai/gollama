a3b88ced85303c0e68c6a16852d11e3905342e4c
Majid Hussain <majidkorai@yahoo.com>
2026-06-02 00:41:48 +0400

    fix: remove misleading Web UI line from gollama run output
    gollama run shows only the llama-server chat URL. The web UI
    requires gollama serve to be running separately.

--- main.go
+++ main.go
@@ -194,9 +194,7 @@ func main() {
 				os.Exit(1)
 			}
 			fmt.Printf("Started %s on port %d (PID %d)\n", inst.Model, inst.Port, inst.PID)
-			ip := localIP()
-			fmt.Printf("Chat:     http://%s:%d\n", ip, inst.Port)
-			fmt.Printf("Web UI:   http://%s:9080 (via gollama serve)\n", ip)
+			fmt.Printf("Chat: http://%s:%d\n", localIP(), inst.Port)
 			fmt.Println("Press Ctrl+C to stop")
 
 			sigCh := make(chan os.Signal, 1)
