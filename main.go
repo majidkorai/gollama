8df0e193a39977c84f8ec7a195ef4eadc23bfa9c
Majid Hussain <majidkorai@yahoo.com>
2026-06-04 20:08:52 +0400

    bump version to v0.2.0

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 
 // version is set at build time via -ldflags=-X main.version=v0.x.x
 // local builds fall back to this default
-var version = "0.1.0-dev"
+var version = "0.2.0"
 
 func main() {
 	if len(os.Args) < 2 || os.Args[1] == "--version" || os.Args[1] == "-v" {
